f64c1b71dbefbc245982bf77b62696ee45a02aae
Rani0086 <shantidhaakad@gmail.com>
2024-08-05 13:47:42 +0530

    Update start.py

--- ROYEDITX/modules/start.py
+++ ROYEDITX/modules/start.py
@@ -112,9 +112,9 @@ async def button(update: Update, context: CallbackContext) -> None:
 ***● ɪ ᴀᴍ waife x ᴀɴɪᴍᴇ♡゙  ᴀɴᴅ ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs.\n\n● ᴀɴɪᴍᴇ ᴠᴇʀsɪᴏɴ ➥ M33.6/V16\n● ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ ➥ 3.11.9\n\n❖ ᴛʜɪs ɪs ᴍᴏsᴛ ᴘᴏᴡᴇʀғᴜʟʟ ᴀɴɪᴍᴇ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴄᴏʟʟᴇᴄᴛ ʜᴀʀᴇᴍ ʙᴏᴛ.***
                """
         keyboard = [
-            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f'http://t.me/{Collectionwaife_bot}?startgroup=new')],
             [InlineKeyboardButton("ᴏᴡɴᴇʀ", url=f'https://t.me/Animefangilrl'),
-             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}')],
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{lolpagalokigc}')],
             [InlineKeyboardButton("ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅs", callback_data='help')],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
